ab3a727e67798d6d029914f7cdc58d7a8a3fe1af
limasuu <szlima.93@gmail.com>
2022-09-16 11:58:37 -0300

    Criação da função cadastrar

--- src/Principal.js
+++ src/Principal.js
@@ -10,12 +10,57 @@ function Principal() {
 
     const [registros, setRegistros]= useState([]);
 
+    const [usuario, setUsuario]= useState('');
+    const [nome, setNome]= useState('');
+    const [email, setEmail]= useState('');
+    const [rua, setRua]= useState('');
+    const [numero, setNumero]= useState('');
+    const [cidade, setCidade]= useState('');
+    const [cep, setCep]= useState('');
+    const [telefone, setTelefone]= useState('');
+    const [empresa, setEmpresa]= useState('');
+
     const carregarDados= async () => {
 
         const dados= await fetch('https://jsonplaceholder.typicode.com/users');
         const vetorDados= await dados.json();
     
         setRegistros(vetorDados);
+    };
+
+    const recomecar= () => {
+
+        setUsuario('');
+        setNome('');
+        setEmail('');
+        setRua('');
+        setNumero('');
+        setCidade('');
+        setCep('');
+        setTelefone('');
+        setEmpresa('');     
+    };
+
+    const cadastrar= () => {
+
+        const novo={
+            'username': usuario,
+            'name': nome,
+            'email': email,
+            'address': {
+                'street': rua,
+                'suite': numero,
+                'city': cidade,
+                'zipcode': cep
+            },
+            'phone': telefone,
+            'company': {
+                'name': empresa
+            }    
+        };
+    
+        setRegistros([novo, ...registros]);
+        recomecar();   
     };
 
     useEffect(() => {
@@ -27,7 +72,14 @@ function Principal() {
             <Cabecalho />
 
             <main>
-                <FichaCadastro />
+                <FichaCadastro cadastrar={cadastrar}
+                    setUsuario={setUsuario} setNome={setNome} setEmail={setEmail} 
+                    setRua={setRua} setNumero={setNumero} setCidade={setCidade}
+                    setCep={setCep} setTelefone={setTelefone} setEmpresa={setEmpresa}
+                    usuario={usuario} nome={nome} email={email} rua={rua}
+                    numero={numero} cidade={cidade} cep={cep} 
+                    telefone={telefone} empresa={empresa}
+                />
                 <Listagem registros={registros} />
             </main>
         </React.Fragment>
--- src/componentes/FichaCadastro.js
+++ src/componentes/FichaCadastro.js
@@ -2,62 +2,64 @@ import React from 'react';
 
 import '../css/FichaCadastro.css';
 
-function FichaCadastro(){
+function FichaCadastro({cadastrar,
+    setUsuario, setNome, setEmail, setRua, setNumero, setCidade, setCep, setTelefone, setEmpresa, 
+    usuario, nome, email, rua, numero, cidade, cep, telefone, empresa}){
 
     return (
         <form>
             <h5>Dados pessoais</h5>
 
             <div className='form-floating'>                
-                <input type='text' placeholder='Nome de usuário' name='usuario' className='form-control bg-dark text-light'/>
+                <input type='text' placeholder='Nome de usuário' name='usuario' value={usuario} onChange={e => setUsuario(e.target.value)} className='form-control bg-dark text-light'/>
                 <label htmlFor='usuario'>Nome de usuário</label>
             </div>
 
             <div className='form-floating'>                
-                <input type='text' placeholder='Nome' name='nome' className='form-control bg-dark text-light'/>
+                <input type='text' placeholder='Nome' name='nome' value={nome} onChange={e => setNome(e.target.value)} className='form-control bg-dark text-light'/>
                 <label htmlFor='nome' className='form-label'>Nome</label>
             </div>
 
             <div className='form-floating'>                
-                <input type='text' placeholder='E-mail' name='email' className='form-control bg-dark text-light'/>
+                <input type='text' placeholder='E-mail' name='email' value={email} onChange={e => setEmail(e.target.value)} className='form-control bg-dark text-light'/>
                 <label htmlFor='email' className='form-label'>E-mail</label>
             </div>
 
             <h5>Dados profissionais</h5>
 
             <div className='form-floating'>                
-                <input type='text' placeholder='Rua' name='rua' className='form-control bg-dark text-light'/>
+                <input type='text' placeholder='Rua' name='rua' value={rua} onChange={e => setRua(e.target.value)} className='form-control bg-dark text-light'/>
                 <label htmlFor='rua' className='form-label'>Rua</label>
             </div>            
 
             <div className='row'>
                 <div className='col form-floating'>
-                    <input type='text' placeholder='Número' name='numero' className='form-control bg-dark text-light'/>
+                    <input type='text' placeholder='Número' name='numero' value={numero} onChange={e => setNumero(e.target.value)} className='form-control bg-dark text-light'/>
                     <label htmlFor='numero' className='form-label'>Número</label>
                 </div>
                 <div className='col form-floating'>                    
-                    <input type='text' placeholder='CEP' name='cep' className='form-control bg-dark text-light'/>
+                    <input type='text' placeholder='CEP' name='cep' value={cep} onChange={e => setCep(e.target.value)} className='form-control bg-dark text-light'/>
                     <label htmlFor='cep' className='form-label'>CEP</label>
                 </div>
             </div>
 
             <div className='form-floating'>                
-                <input type='text' placeholder='Cidade' name='cidade' className='form-control bg-dark text-light'/>
+                <input type='text' placeholder='Cidade' name='cidade' value={cidade} onChange={e => setCidade(e.target.value)} className='form-control bg-dark text-light'/>
                 <label htmlFor='cidade' className='form-label'>Cidade</label>
             </div>
 
             <div className='row'>
                 <div className='col form-floating'>
-                    <input type='text' placeholder='Telefone' name='telefone' className='form-control bg-dark text-light'/>
+                    <input type='text' placeholder='Telefone' name='telefone' value={telefone} onChange={e => setTelefone(e.target.value)} className='form-control bg-dark text-light'/>
                     <label htmlFor='telefone' className='form-label'>Telefone</label>
                 </div>
                 <div className='col form-floating'>                    
-                    <input type='text' placeholder='Empresa' name='empresa' className='form-control bg-dark text-light'/>
+                    <input type='text' placeholder='Empresa' name='empresa' value={empresa} onChange={e => setEmpresa(e.target.value)} className='form-control bg-dark text-light'/>
                     <label htmlFor='empresa' className='form-label'>Empresa</label>
                 </div>
             </div>  
 
-            <input type='button' value='Cadastrar' className='btn btn-primary form-control'/>
+            <input type='button' value='Cadastrar' className='btn btn-primary form-control' onClick={cadastrar}/>
                         
         </form>
     );
